55cb57add405bed7555fd46fa2bccbd0a3090589
alephanta <alephanta@gmail.com>
2024-06-06 22:35:48 +0400

    8-9-10-06.06.2024

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,15 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import { deleteAsync } from 'del';
 
 // Styles
 
@@ -12,18 +19,112 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
 
-// Server
+export const headerFooterStyles = () => {
+  return gulp.src(['source/less/header.less', 'source/less/footer.less'], { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(rename((path) => {
+      if (path.basename === 'header') {
+        path.basename = 'header.min';
+      } else if (path.basename === 'footer') {
+        path.basename = 'footer.min';
+      }
+    }))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    .pipe(browser.stream());
+}
 
-const server = (done) => {
+// HTML
+
+export const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+// Images
+export const copyImages = () => {
+  return gulp.src(['source/img/**/*.{jpg,png}'])
+    .pipe(gulp.dest('build/img'));
+}
+
+export const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'));
+}
+
+// WebP
+export const createWebp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh({
+      webp: {},
+    })
+  )
+    .pipe(gulp.dest('build/img'));
+}
+
+// SVG
+export const svg = () => {
+  return gulp.src(['source/img/description-items/*.svg', 'source/img/form/*.svg', '!source/img/icons/*.svg'])
+  .pipe(svgo())
+  .pipe(gulp.dest('build/img'));
+}
+
+// Sprite
+export const sprite = () => {
+  return gulp.src(['source/img/icons/*.svg'])
+  .pipe(svgo())
+  .pipe(svgstore( {
+    inlineSvg: true
+  }))
+  .pipe(rename('autosprite.svg'))
+  .pipe(gulp.dest('build/img'));
+}
+
+// Fonts Webmanifest Favicons
+export const copy = (done) => {
+  gulp.src([
+    'source/fonts/lato/*.{woff2,woff}',
+    'source/fonts/oswald/*.{woff2,woff}',
+    'source/*.ico',
+    'source/*.webmanifest',
+    'source/img/sprite/*.{svg}'
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'))
+  done();
+}
+
+// Clean
+export const clean = () => {
+  return deleteAsync('build');
+}
+
+// Reload
+export const reload = (done) => {
+  browser.reload();
+  done();
+}
+
+// Server
+export const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -34,13 +135,39 @@ const server = (done) => {
 }
 
 // Watcher
-
-const watcher = () => {
+export const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+// Build
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    headerFooterStyles,
+    html,
+    svg,
+    sprite,
+    createWebp
+  ),
+);
 
 export default gulp.series(
-  styles, server, watcher
-);
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    headerFooterStyles,
+    html,
+    svg,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
